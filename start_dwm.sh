83cc4f6ce3e2735713e573e40137b7cd5babc43b
Sy Tran Dung <sytrancsvn@gmail.com>
2024-11-18 07:26:56 +0700

    do not play sound

--- start_dwm.sh
+++ start_dwm.sh
@@ -3,6 +3,6 @@ xcompmgr -c -fF -I-.002 -O-.003 -D1 &
 slstatus &
 xautolock -time 3 -locker slock -corners ---- \
 	-notify 15 \
-	-notifier "espeak 'Screen will be locked'; notify-send -u critical -t 15000 'Autolocking' 'The screen will be locked in 15s'"  &
+	-notifier "notify-send -u critical -t 15000 'Autolocking' 'The screen will be locked in 15s'"  &
 dunst &
 dwm
